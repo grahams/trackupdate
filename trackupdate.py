b0ced731f3ace436b3b91ca91565b2b0791d32d6
grahams <github@sean-graham.com>
2010-08-26 23:50:30 -0400

    updated copyright dates and added license to plugin files

--- trackupdate.py
+++ trackupdate.py
@@ -1,6 +1,6 @@
 #!/opt/local/bin/python
 
-# Copyright (c) 2009 Sean M. Graham <www.sean-graham.com>
+# Copyright (c) 2009-2010 Sean M. Graham <www.sean-graham.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
