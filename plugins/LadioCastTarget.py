98d3bc1289c877dc2b6eb9c22d07c1f0fec9a570
seanth <seanth@gmail.com>
2018-03-02 12:29:21 -0600

    New plugin for updating track info using LadioCast

---
+++ plugins/LadioCastTarget.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2018 Sean T. Hammond
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from Target import Target
+import ConfigParser
+import os
+
+class LadioCastTarget(Target):
+    pluginName = "LadioCast Track Updater"
+
+    initTitle = ""
+    initArtist = ""
+    initAlbum = ""
+    initTime = ""
+
+    def __init__(self, config, episode):
+        try:
+            self.initTitle = config.get('LadioCastTarget', 'initTitle')
+            self.initArtist = config.get('LadioCastTarget', 'initArtist')
+            self.initAlbum = config.get('LadioCastTarget', 'initAlbum')
+            self.initTime = config.get('LadioCastTarget', 'initTime')
+        except ConfigParser.NoSectionError:
+            print("LadioCastTarget: No [LadioCastTarget] section in config")
+            return
+        except ConfigParser.NoOptionError:
+            print("LadioCastTarget: Missing values in config")
+            return
+
+    def close(self):
+        return
+
+    def logTrack(self, title, artist, album, time, startTime):
+        cmd = """osascript -e 'tell app "Finder" to sleep'"""
+        theCmd = """osascript -e 'tell application "LadioCast" to set metadata song to title & " — " & artist & " — " & time'"""
+        os.system(theCmd)
+        
+
