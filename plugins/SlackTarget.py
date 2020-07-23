218f1fe85285fb12d5e17cf04a6d00b86e8d9a7b
Sean Graham <github@sean-graham.com>
2020-07-23 16:49:22 -0400

    slack: include cover art (and more formatting)

--- plugins/SlackTarget.py
+++ plugins/SlackTarget.py
@@ -25,38 +25,107 @@ import string
 import subprocess
 import json
 import requests
+import logging
+import uuid
+import time
 
 class SlackTarget(Target):
     pluginName = "Slack Track Updater"
+    logger = logging.getLogger("slack updater")
 
     # config values
     slackChannel = "" #what slack channel do you want the track data announced in
     slackWebHookUrl = "" #this is the slack webhook url. See https://api.slack.com/incoming-webhooks for more info
-    slackEmoji = "" #you can prefix the text sent to slack with an emoji. it only appears if the viewer's message display is set to "clean" in preferences
     slackAnnouncementPrefix = "" #this should be the "user name", but we're using it almost like moo-like a stage-talk
 
     def __init__(self, config, episode):
-        self.theJSONPayload = '{"channel": "%s", "username": %s, "text": "%s", "icon_emoji": ":%s:"}'
         try:
             self.slackChannel = config.get('SlackTarget', 'slackChannel')
             self.slackWebHookUrl = config.get('SlackTarget', 'webhookURL')
-            self.slackEmoji = config.get('SlackTarget', 'emojiName')
             self.slackAnnouncementPrefix = config.get('SlackTarget', 'announcementPrefix')
         except configparser.NoSectionError:
-            print("SlackTarget: No [SlackTarget] section in config")
+            self.logger.error("SlackTarget: No [SlackTarget] section in config")
             return
         except configparser.NoOptionError:
-            print("SlackTarget: Missing values in config")
+            self.logger.error("SlackTarget: Missing values in config")
             return
 
     def close(self):
         return
 
-    def logTrack(self, title, artist, album, time, artwork, startTime, ignore):
-        if( ignore is not True):
-            payload = {'channel': self.slackChannel,
-                       'username': self.slackAnnouncementPrefix,
-                       'text': f'_{title}_ by {artist}',
-                       'icon_emoji': self.slackEmoji}
+    def logTrack(self, title, artist, album, length, artwork, startTime, ignore):
 
-            requests.post(self.slackWebHookUrl, json=payload)
+        artworkUrl = None
+        postString = f'{title} by {artist}'
+
+        blocks = [{
+            "type": "section",
+            "block_id": str(uuid.uuid4()),
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*{self.slackAnnouncementPrefix}*" 
+            },
+            "fields": [
+            {
+                    "type": "mrkdwn",
+                    "text": "*Song*"
+            },
+            {
+                    "type": "mrkdwn",
+                    "text": title
+            },
+            {
+                    "type": "mrkdwn",
+                    "text": "*Artist*"
+            },
+            {
+                    "type": "mrkdwn",
+                    "text": artist
+            },
+            {
+                    "type": "mrkdwn",
+                    "text": "*Album*"
+            },
+            {
+                    "type": "mrkdwn",
+                    "text": album
+            }
+            ]
+        }]
+
+        if(artwork.find("Public") > -1):
+            artworkStem = artwork.split("Public")[1]
+            artworkUrl = f"http://grahams.wtf/public/{artworkStem}"
+
+        if(artworkUrl is not None):
+            # Since it takes awhile for the images to propogate, let's make
+            # sure they exist first
+            waitingUpload = True
+            failureCounter = 5
+
+            while(waitingUpload and (failureCounter > 0) ):
+                imgRequest = requests.get(artworkUrl)
+
+                self.logger.debug("imgRequest status: " + str(imgRequest.status_code))
+
+                if(imgRequest.status_code == 404):
+                    self.logger.debug(f"Cover Image 404: 5s Delay. {failureCounter} left")
+                    failureCounter = failureCounter - 1;
+                    time.sleep(5);
+                else:
+                    blocks[0]["accessory"] = {
+                                "type": "image",
+                                "image_url": artworkUrl,
+                                "alt_text": f"Cover image of {postString}"
+                    }
+                    failureCounter = -1
+                    waitingUpload = False
+
+        if( ignore is not True):
+            payload = { 'text': postString, 'blocks': blocks }
+
+            self.logger.debug("sending payload: " + json.dumps(payload))
+
+            r = requests.post(self.slackWebHookUrl, json=payload)
+            self.logger.debug("post status: " + str(r.status_code))
+            self.logger.debug("post response: " + str(r.text))
